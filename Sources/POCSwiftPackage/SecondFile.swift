e55d3c96089766b171977e831d82c8ec77424f3e
Manjunatha Kaliwal <manjunatha.kaliwal@delta.com>
2023-07-12 16:12:45 -0400

    2nd file

---
+++ Sources/POCSwiftPackage/SecondFile.swift
@@ -0,0 +1,15 @@
+//
+//  SecondFile.swift
+//  
+//
+//  Created by Manjunatha Kaliwal on 7/12/23.
+//
+
+import UIKit
+
+open class SecondFile: NSObject {
+
+    open func firstFunction() {
+        print("First file")
+    }
+}
