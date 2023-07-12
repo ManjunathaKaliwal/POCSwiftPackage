ce9668493bea5d9819347be1b2043023e452b0c3
Manjunatha Kaliwal <manjunatha.kaliwal@delta.com>
2023-07-12 16:02:55 -0400

    Initial Commit

---
+++ Sources/POCSwiftPackage/FirstSwiftFile.swift
@@ -0,0 +1,14 @@
+//
+//  FirstSwiftFile.swift
+//  
+//
+//  Created by Manjunatha Kaliwal on 7/12/23.
+//
+
+import UIKit
+
+open class FirstSwiftFile: NSObject {
+    open func firstFunction() {
+        print("First file")
+    }
+}
